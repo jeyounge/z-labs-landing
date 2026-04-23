777ea8a62a6ac54a1b4b4687a8fee1cadc503a7b
제키_우제영 <jeyounge@afreecatv.com>
2026-04-23 16:25:05 +0900

    chore: remove .next from git tracking and add to .gitignore

--- .next/static/chunks/app/privacy/page-b65e3b336cb11d73.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[66,877],{2232:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=2232)),_N_E=e.O()}]);
